f66204ba4486a947d4156543af955afb9a7a6778
Hojaverde <37783633+unawea1@users.noreply.github.com>
2018-07-06 12:31:43 -0400

    Previo a actualizar Shadero 1.9

--- Assets/Scripts/PanelesInventario.cs
+++ Assets/Scripts/PanelesInventario.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Events;
 
 public class PanelesInventario : MonoBehaviour
 {
 
     private CanvasGroup canvasGroup;
-    private bool abierto = true;
+    private bool abierto = false;
     public static PanelesInventario panelesInventario;
     public ToolTip tooltipObjetos;
     public GameObject tooltipEquipo;
     private Vector3 relativePos;
+    public UnityEvent OnCerrar;
+    public UnityEvent OnAbrir;
     private void Awake()
     {
         panelesInventario = this;
@@ -20,20 +23,23 @@ public class PanelesInventario : MonoBehaviour
 
     public void AbrirCerrarInventario()
     {
+        
         if (abierto)
         {
+            OnCerrar?.Invoke();
             Debug.Log("Desactivando Inventario");
             canvasGroup.interactable = false;
-            canvasGroup.alpha = 0;
+            //canvasGroup.alpha = 0;
             canvasGroup.blocksRaycasts = false;
             canvasGroup.ignoreParentGroups = true;
             abierto = false;
         }
         else
         {
+            OnAbrir?.Invoke();
             Debug.Log("Activando Inventario");
             canvasGroup.interactable = true;
-            canvasGroup.alpha = 1;
+            //canvasGroup.alpha = 1;
             canvasGroup.blocksRaycasts = true;
             canvasGroup.ignoreParentGroups = true;
             abierto = true;
---
+++ Assets/panelRenderPrueba.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class panelRenderPrueba : MonoBehaviour {
+
+    private CanvasRenderer miRender;
+
+    private void Awake()
+    {
+        miRender = GetComponent<CanvasRenderer>();
+        
+    }
+
+    IEnumerator FadeOut()
+    {
+        Debug.Log("Ejecutando shader");
+        for (float i = 0; i <= 1; i+=0.03f)
+        {
+            miRender.GetMaterial().SetFloat("_Destroyer_Value_1", i);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    IEnumerator FadeIn()
+    {
+        Debug.Log("Ejecutando shad");
+        for (float i = 1; i >= 0; i -= 0.03f)
+        {
+            miRender.GetMaterial().SetFloat("_Destroyer_Value_1", i);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+
+    public void DesvanecerUI()
+    {
+        StartCoroutine(FadeOut());
+    }
+
+    public void AparecerUI()
+    {
+        StartCoroutine(FadeIn());
+    }
+}
